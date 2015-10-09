c3a0ca2c3d67b16b34132ba909a7730cff2564ee
Ten Wong <wangtengoo7@gmail.com>
2015-10-09 18:17:40 +0800

    Added database

--- android/app/src/main/java/cc/seeed/iot/MyApplication.java
+++ android/app/src/main/java/cc/seeed/iot/MyApplication.java
@@ -1,19 +1,35 @@
 package cc.seeed.iot;
 
-import android.app.Application;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.login.LoginException;
 
 import cc.seeed.iot.datastruct.User;
 import cc.seeed.iot.webapi.IotApi;
+import cc.seeed.iot.webapi.IotService;
+import cc.seeed.iot.webapi.model.GroverDriver;
 import cc.seeed.iot.webapi.model.Node;
+import cc.seeed.iot.webapi.model.NodeListResponse;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by tenwong on 15/7/9.
  */
-public class MyApplication extends Application {
+public class MyApplication extends com.activeandroid.app.Application {
     private SharedPreferences sp;
 
     private ArrayList<Node> nodes = new ArrayList<Node>();
@@ -100,9 +116,104 @@ public class MyApplication extends Application {
         configState = sp.getBoolean("loginState", false);
 
         init();
+
+        getGrovesData();
+
+        getNodesData();
     }
 
     private void init() {
         IotApi.SetServerUrl(server_url);
+    }
+
+    public void getGrovesData() {
+        IotApi api = new IotApi();
+        String token = user.user_key;
+        api.setAccessToken(token);
+        IotService iot = api.getService();
+        iot.scanDrivers(new Callback<List<GroverDriver>>() {
+            @Override
+            public void success(List<GroverDriver> groverDrivers, retrofit.client.Response response) {
+                for (GroverDriver groveDriver : groverDrivers) {
+                    groveDriver.save();
+//                    new DownloadImageAsyncTask().execute(groveDriver);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e(getClass().getName(), error.toString());
+            }
+        });
+    }
+
+    public void getNodesData() {
+        IotApi api = new IotApi();
+        String token = user.user_key;
+        api.setAccessToken(token);
+        final IotService iot = api.getService();
+        iot.nodesList(new Callback<NodeListResponse>() {
+            @Override
+            public void success(NodeListResponse nodeListResponse, Response response) {
+                if (nodeListResponse.status.equals("200")) {
+                    nodes = (ArrayList) nodeListResponse.nodes;
+                    for (Node node : nodes) {
+                        node.save();
+                    }
+                } else {
+                    Log.e(getClass().getName(), nodeListResponse.msg);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e(getClass().getName(), error.toString());
+            }
+        });
+    }
+
+
+    class DownloadImageAsyncTask extends AsyncTask<GroverDriver, String, Void> {
+
+        @Override
+        protected Void doInBackground(GroverDriver... groverDrivers) {
+            Bitmap bitmap = null;
+            for (GroverDriver groverDriver : groverDrivers) {
+                try {
+                    URL imageUrl = groverDriver.ImageURL;
+                    imageUrl.openConnection();
+                    bitmap = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream());
+                } catch (Exception e) {
+                    Log.e(getClass().getName(), e.toString());
+                }
+
+                saveToInternalSorage(bitmap, Integer.toString(groverDriver.ID));
+            }
+
+            return null;
+        }
+    }
+
+    private String saveToInternalSorage(Bitmap bitmapImage, String fileName) {
+        final String DIR = "grove";
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir(DIR, Context.MODE_PRIVATE);
+        Log.e(getClass().getName(), directory.getPath());
+        // Create imageDir
+        File file = new File(directory, fileName);
+
+        FileOutputStream fos = null;
+        try {
+
+            fos = new FileOutputStream(file);
+
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return directory.getAbsolutePath();
     }
 }
--- android/app/src/main/java/cc/seeed/iot/webapi/model/GroverDriver.java
+++ android/app/src/main/java/cc/seeed/iot/webapi/model/GroverDriver.java
@@ -1,5 +1,9 @@
 package cc.seeed.iot.webapi.model;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
@@ -7,19 +11,42 @@ import java.util.Map;
 /**
  * Created by tenwong on 15/6/25.
  */
-public class GroverDriver {
+@Table(name = "groves")
+public class GroverDriver extends Model {
     public List<String> Files;
+
     public Map<String, List<String>> Inputs;
+
+    @Column(name = "class_file")
     public String ClassFile;
+
+    @Column(name = "grove_name")
     public String GroveName;
+
     public Map<String, List<String>> Outputs;
+
+    @Column(name = "image_url")
     public URL ImageURL;
+
     public List<String> Events;
+
+    @Column(name = "class_name")
     public String ClassName;
+
+    @Column(name = "can_get_last_error")
     public Boolean CanGetLastError;
+
+    @Column(name = "interface_type")
     public String InterfaceType;
+
+    @Column(name = "has_event")
     public Boolean HasEvent;
+
+    @Column(name = "include_path")
     public String IncludePath;
+
+    @Column(name = "grove_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
     public int ID;
+
     public List<String> ConstructArgList;
 }
